e8c6a29049fd0fb1016267723e8e7c94f1f5d7da
CPT-Dawn <dawnsp0456@gmail.com>
2026-03-10 03:48:34 +0530

    Wezterm Remove upper tab

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,6 +2,12 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-
+config = {
+    automatically_reload_config = true,
+    enable_tab_bar = false,
+    window_close_confirmation = "NeverPrompt",
+    window_decoration = "RESIZE",
+    default_cursor_style = "BlinkingBar",
+}
 
 return config
