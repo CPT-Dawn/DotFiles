04592e6e90012eb45f2ca0e5bbaebb638fadfd80
CPT-Dawn <dawnsp0456@gmail.com>
2026-03-13 06:29:09 +0530

    Wezterm TUI buff

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,6 +9,12 @@ local config = wezterm.config_builder()
 config.enable_tab_bar = false
 config.window_decorations = "NONE"
 config.window_close_confirmation = "NeverPrompt"
+config.window_padding = {
+    left = 12,
+    right = 12,
+    top = 12,
+    bottom = 12,
+}
 
 -- ==========================================================
 -- Font Configuration
@@ -25,12 +31,15 @@ config.font = wezterm.font_with_fallback({
 -- Cursor Style & Key Binds
 -- ==========================================================
 
-config.default_cursor_style = "BlinkingBlock"
-config.cursor_blink_rate = 500
+config.default_cursor_style = "BlinkingBar"
+config.cursor_thickness = "1.5pt"
 config.disable_default_key_bindings = true
 config.keys = {
     { key = 'c', mods = 'CTRL|SHIFT', action = wezterm.action.CopyTo('Clipboard') },
     { key = 'v', mods = 'CTRL|SHIFT', action = wezterm.action.PasteFrom('Clipboard') },
+    { key = '+', mods = 'CTRL', action = wezterm.action.IncreaseFontSize },
+    { key = '-', mods = 'CTRL', action = wezterm.action.DecreaseFontSize },
+    { key = '0', mods = 'CTRL', action = wezterm.action.ResetFontSize },
 }
 
 return config
